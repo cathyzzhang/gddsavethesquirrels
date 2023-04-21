5000f94e75e3a6478da4687eaec3c8525c69703e
cathyzzhang <62196431+cathyzzhang@users.noreply.github.com>
2023-04-20 19:11:41 -0700

    4/20 update

---
+++ SaveTheSquirrels/Assets/BasketballGame/scripts/ScoreCounter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    private int score;
+    [SerializeField]
+    private Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scoreText.text = "Score: " + score;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Acorn"))
+        {
+            float scoreTime = Time.time;
+            score += 1;
+            scoreText.text = "Score: " + score;
+
+        }
+    }
+}
---
+++ SaveTheSquirrels/Assets/EventManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EventManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int maxDistance = 500;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //clicking on join switches scenes to different minigames that corresponds to different event IDs
+    public void ActivateEvent(int eventID)
+    {
+        if (eventID == 1) //Doe Library & Maze Game
+        {
+            Debug.Log("Loading Maze Game");
+        } else if (eventID == 2) //Cal Memorial Stadium & Basketball Game
+        {
+            Debug.Log("Loading Basketball Game");
+            SceneManager.LoadScene("EnterBasketball");
+
+        } else if (eventID == 3) //Campanile & Car Race Game
+        {
+            Debug.Log("Loading Car Race Game");
+        } else if (eventID == 4) //Jacobs Hall & Shooting Game
+        {
+            SceneManager.LoadScene("EnterShoot");
+        }
+    }
+
+
+}
